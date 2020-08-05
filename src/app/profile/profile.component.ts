589e57387279e03a910cd21fcdfd0e325a8aaa06
Bhaskar Sen <bhaskarsen1998@gmail.com>
2020-08-06 02:32:49 +0530

    update profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any = {};
+  posts: any[] = [];
+ 
+  constructor(public activatedRoute: ActivatedRoute) { 
 
-  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+
+    this.getProfile(id);
+    this.getUsersPosts(id);
+
+  }
+
+  ngOnInit() {
+  }
+
+  getProfile(id: string){
+
+    firebase.firestore().collection("users").doc(id).get().then((documentSnapshot) => {
+
+      this.user = documentSnapshot.data();
+      this.user.displayName = this.user.firstName + " " + this.user.lastName;
+      this.user.id = documentSnapshot.id;
+      this.user.hobbies = this.user.hobbies.split(",");
+      console.log(this.user);
+
+    }).catch((error) => {
+      console.log(error);
+    })
+
+  }
+
+  getUsersPosts(id: string){
+    firebase.firestore().collection("posts")
+    .where("owner","==", id).get().then((data)=>{
+      
+      this.posts = data.docs;
+
+    })
   }
 
 }
